428f41ae3befaf10c9cad5db40a0f6377656fba8
Francois Romain <francoisromain@gmail.com>
2018-03-26 10:06:31 +0200

    fix: indentation

--- query.sql
+++ query.sql
@@ -5,13 +5,13 @@ SELECT
 	statut.nom_etatj statut,
 	travaux.nom_etatt travaux,
 	json_build_object(
-    'principales', substances,
-    'connexes', null
-  ) substances,
+		'principales', substances,
+		'connexes', null
+	) substances,
 	json_build_object('durée', titre.duree, 'début', titre.datmo) validité,
 	emprises.nom_locmer emprises,
-  titre.surf surface,
-  titulaires_json titulaires,
+	titre.surf surface,
+	titulaires_json titulaires,
 	json_build_object('statut', statuts) démarches 
 FROM 
 	titres_deb.m_t_titre titre
@@ -45,18 +45,18 @@ LEFT JOIN
 LEFT JOIN
 	titres_deb.m_r_locmer emprises
 	ON 
-    CAST(emprises.id AS text) = titre.locmer 
+		CAST(emprises.id AS text) = titre.locmer 
 LEFT JOIN
-  (SELECT
-    titulaire.id_tit titulaire_titre_id,
-    json_agg(titulaire.id) titulaires_json
-  FROM
-    titres_deb.m_t_titu titulaire
-  GROUP BY
-    titulaire.id_tit
-  ) titu
+	(SELECT
+		titulaire.id_tit titulaire_titre_id,
+		json_agg(titulaire.id) titulaires_json
+	FROM
+		titres_deb.m_t_titu titulaire
+	GROUP BY
+		titulaire.id_tit
+	) titu
 	ON 
-    titulaire_titre_id = titre.id
+		titulaire_titre_id = titre.id
 LEFT JOIN
 	(SELECT
 		stat.id_tit statut_titre_id,
@@ -64,7 +64,7 @@ LEFT JOIN
 			'id', stat.id,
 			'type', statut_type.typ_aff,
 			'statut', statut_etat.etat_aff,
-      'description', stat.nom,
+			'description', stat.nom,
 			'dates', json_build_object(
 				'ouverture', stat.dat_a_ouv,
 				'fermeture', stat.dat_a_fer 
@@ -85,36 +85,36 @@ LEFT JOIN
 		(SELECT
 			evt.id_aff evt_id_aff,
 			json_agg(json_build_object(
-        'id', evt.id,
-        'nom', evt_type.evt_nom,
-        'date', evt.dat,
-        'acte', false,
-        'public', false,
-        'organisme', null,
-        'fichier', file_json,
-        'commentaire', evt.obs,
-        'étiquettes', null
-      )) documents_json
+				'id', evt.id,
+				'nom', evt_type.evt_nom,
+				'date', evt.dat,
+				'acte', false,
+				'public', false,
+				'organisme', null,
+				'fichier', file_json,
+				'commentaire', evt.obs,
+				'étiquettes', null
+			)) documents_json
 		FROM
-      titres_deb.m_t_evt evt
-    LEFT JOIN
-      titres_deb.m_r_evt evt_type
-      ON
-        evt_type.id = evt.cod_evt
-    INNER JOIN
-      (SELECT
-        file.id_evt file_id_evt,
-        json_build_object(
-          'id', file.id,
-          'url', file.url,
-          'taille', file.taille,
-          'type', file.exten 
-        ) file_json
-      FROM
-        titres_deb.m_t_docu file
-      ) fils
-      ON
-        file_id_evt = evt.id
+			titres_deb.m_t_evt evt
+		LEFT JOIN
+			titres_deb.m_r_evt evt_type
+			ON
+				evt_type.id = evt.cod_evt
+		INNER JOIN
+			(SELECT
+				file.id_evt file_id_evt,
+				json_build_object(
+					'id', file.id,
+					'url', file.url,
+					'taille', file.taille,
+					'type', file.exten 
+				) file_json
+			FROM
+				titres_deb.m_t_docu file
+			) fils
+			ON
+				file_id_evt = evt.id
 		GROUP BY
 			evt.id_aff
 		) docs
